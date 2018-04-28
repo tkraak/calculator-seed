9110be561b36e951e99a7723ac8c72485e38c457
Tom Kraak <tom@backroom.io>
2018-04-28 14:03:23 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PrettierPlugin = require('prettier-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Calculator',
+    }),
+    new PrettierPlugin({
+      printWidth: 68,
+      singleQuote: true,
+      trailingComma: 'es5',
+    }),
+  ],
+  devServer: {
+    stats: 'errors-only',
+    host: process.env.HOST, // defaults to `localhost`
+    port: process.env.PORT, // defaults to 8080
+    open: false,
+    overlay: true,
+  },
+};
