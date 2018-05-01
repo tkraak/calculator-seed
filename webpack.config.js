177eb3f8a2e6f65f560a23e5e1a4c1981c3677c1
Tom Kraak <thomas.kraak@teamaol.com>
2018-05-01 15:38:50 -0400

    transform modern syntax to browser-compatible JavaScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const PrettierPlugin = require('prettier-webpack-plugin');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
