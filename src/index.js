de5733cbd3402e717ee4f2e14e1e01b2026c3793
Tom Kraak <thomas.kraak@teamaol.com>
2018-05-25 17:24:17 -0400

    turn repo into seed

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys');
+// your JavaScript goes here :)-
 
-keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
-    const key = e.target;
-    const action = key.dataset.action;
-
-    if (!action) {
-      console.log('number key!');
-    }
-
-    if (
-      action === 'add' ||
-      action === 'subtract' ||
-      action === 'multiply' ||
-      action === 'divide'
-    ) {
-      console.log('operator key!');
-    }
-
-    if (action === 'decimal') {
-      console.log('decimal key!');
-    }
-
-    if (action === 'clear') {
-      console.log('clear key!');
-    }
-
-    if (action === 'calculate') {
-      console.log('equal key!');
-    }
-  }
-});
+console.log('You can use ES6 here, yay!');
